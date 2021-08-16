4b9a0020642c30156e2ab521b9e592d530744228
EC2 Default User <ec2-user@ip-10-0-2-74.ec2.internal>
2021-08-16 08:54:32 +0000

    update

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_5XJZGVNWA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2kc72p7eic3gi74b2ue1513utq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
