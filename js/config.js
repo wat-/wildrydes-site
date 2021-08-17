1991bb824d1db9aa520cdbdf5e4c8ce67cfdb8b5
watd <wataru.s06140@gmail.com>
2021-08-17 13:42:41 +0900

    update

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_5XJZGVNWA', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2kc72p7eic3gi74b2ue1513utq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_rZMo6BTyE', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1vavhqfgchnf7us34e1tm46b7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://vpbem56y36.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
